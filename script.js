bf08e927965d102dcd7d3e9248f19cd16a5de15d
pcorderogil-art <pcorderogil@gmail.com>
2025-11-24 18:23:21 +0100

    cambios

---
+++ script.js
@@ -0,0 +1,47 @@
+// BOTÓN SUBIR
+const btn = document.getElementById("btnSubir");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 400) {
+        btn.style.display = "flex";
+    } else {
+        btn.style.display = "none";
+    }
+});
+
+btn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+
+// CARGA INFINITA DE BLOQUES (LÍMITE 10)
+const container = document.getElementById("contenido");
+let contador = 0;
+const limite = 10;
+
+function cargarBloques() {
+    if (contador >= limite) return;
+
+
+        const bloque = document.createElement("div");
+        bloque.className = "bloque";
+        bloque.innerHTML = `
+            <h3>Bloque ${contador}</h3>
+            
+        `;
+        container.appendChild(bloque);
+        contador++;
+    }
+
+
+// scroll infinito controlado
+window.addEventListener("scroll", () => {
+    const bottom = window.innerHeight + window.scrollY >= document.body.offsetHeight - 50;
+
+    if (bottom && contador < LIMITE) {
+        cargarBloques();
+    }
+});
